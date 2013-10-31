509c39ad8ba392697c271818d2103de24e590798
Alex <alexander.panfilenok@gmail.com>
2013-10-31 08:51:56 -0700

    Add Leson05

---
+++ Lesson05/Controllers/PostController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lesson05.Controllers
+{
+    public class PostController : Controller
+    {
+        //
+        // GET: /Post/
+
+        public ActionResult Index(string seoUrl)
+        {
+            return View();
+        }
+
+    }
+}
