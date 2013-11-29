b9eb6c80cc91d9bda1c15d5bb04e077bf2662322
lAnubisl <alexander.panfilenok@gmail.com>
2013-11-29 09:15:20 +0300

    commit changes

---
+++ Lesson04/Models/User.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Lesson04.Models
+{
+    public class User
+    {
+        public long UserId { get; set; }
+
+        public string UserName { get; set; }
+
+        public string Password { get; set; }
+
+        public long RoleId { get; set; }
+    }
+
+    public class Role
+    {
+        public long RoleId { get; set; }
+
+        public string Name { get; set; }
+    }
+}
